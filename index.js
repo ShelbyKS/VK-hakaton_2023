46bab92abd8fbbbbfa32553e43c0d6c0ddf66524
maffin1799 <nik.chujko@mail.ru>
2023-11-25 17:36:55 +0300

    rr

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
-import { getAddSurvey } from './api';
+
 import './index.css';
+
+
+const API_PATH = '/api/v1';
+
+export const getAddSurvey = (key, value) => {
+    console.log(key, value)
+    return fetch(`${API_PATH}/survey/add`, {
+        method: 'POST', headers: {
+            'Content-Type': 'application/json;charset=utf-8',
+        }, credentials: 'include',
+        body: JSON.stringify({
+            attribute: key,
+            metric: value,
+        }),
+    }).then(response => {
+        if (!response.ok) {
+            throw new Error('Ответ сервера не 200');
+        }
+
+        return response.json();
+    })
+        .then(data => {
+            return data;
+        })
+        .catch(error => {
+            throw new Error(error);
+        });
+};
 
 const handleClick = (element) => {
 
